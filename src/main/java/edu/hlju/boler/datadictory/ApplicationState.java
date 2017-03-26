0fa1ac4a2b11a9273af9b6fe5460b61b914d2c6c
jingqingyun <442932697@qq.com>
2017-03-27 01:49:10 +0800

    添加求职申请的数据字典

---
+++ src/main/java/edu/hlju/boler/datadictory/ApplicationState.java
@@ -0,0 +1,13 @@
+package edu.hlju.boler.datadictory;
+
+public enum ApplicationState {
+    APPLIED,
+    VIEWED,
+    APPLY_REFUSED,
+    INTERVIEW_INVITED,
+    INVITATION_REFUSED,
+    SENCOND_INTERVIEW,
+    THIRD_INTERVIEW,
+    OFFER,
+    OFFER_REFUSED;
+}
