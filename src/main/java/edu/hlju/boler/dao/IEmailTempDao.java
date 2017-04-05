4a7ccd269eed34394cbc8aba16d633ebb25db571
jingqingyun <qingyun.jing@8lab.cn>
2017-04-05 15:24:00 +0800

    添加按Employ查找的接口

--- src/main/java/edu/hlju/boler/dao/IEmailTempDao.java
+++ src/main/java/edu/hlju/boler/dao/IEmailTempDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.cache.annotation.Cacheable;
 
 import edu.hlju.boler.pojo.po.EmailTemplate;
+import edu.hlju.boler.pojo.po.User;
 
 public interface IEmailTempDao {
 
@@ -19,10 +20,13 @@ public interface IEmailTempDao {
     List<EmailTemplate> selectAll() throws SQLException;
 
     @Cacheable
+    List<EmailTemplate> selectByEmploy(User employ) throws SQLException;
+
+    @Cacheable
     EmailTemplate selectById(Integer id) throws SQLException;
 
     int updateById(EmailTemplate record) throws SQLException;
 
     int updateByIdSelective(EmailTemplate record) throws SQLException;
-    
+
 }
